7c8a98583931627a33d911c58721aa4fc498266e
UlrikKryhl <uchris23@student.aau.dk>
2025-04-14 13:19:03 +0200

    okay cool
    yay

--- node/PublicResources/js/notes.js
+++
@@ -1,9 +0,0 @@
-// Select button and sidebar
-const toggleButton = document.getElementById('toggle-button');
-const sidebar = document.getElementById('sidebar');
-
-toggleButton.addEventListener('click', () => {
-    // Toggle the sidebar's visibility
-    sidebar.classList.toggle('active');
-});
-
