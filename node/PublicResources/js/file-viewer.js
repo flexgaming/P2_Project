d5bda9a2ed51087bc838c4777f79452230b7632f
flexgaming <skinkelynet06@gmail.com>
2025-05-05 17:15:29 +0200

    Minor setup

---
+++ node/PublicResources/js/file-viewer.js
@@ -0,0 +1,38 @@
+
+/* **************************************************
+                    Impot & Export
+   ************************************************** */
+
+export {  };
+import {  } from './server.js';
+import {  } from './app.js';
+
+import fs from 'fs'; // https://nodejs.org/en/learn/manipulating-files/working-with-folders-in-nodejs
+
+
+// Upload File
+
+
+
+// Download File
+
+
+
+// Move File
+
+
+
+// Rename File
+
+
+
+// Delete File
+
+
+
+// Select File
+
+
+
+// Navigate the file path
+
