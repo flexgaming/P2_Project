7ec4778513940106a1bf913dc7ccee884b59a3af
flexgaming <skinkelynet06@gmail.com>
2025-05-06 16:23:04 +0200

    code in app
    instead of file viewer, the code is now inside app.js

--- node/PrivateResources/app.js
+++ node/PrivateResources/app.js
@@ -267,3 +267,133 @@ function parseCookies(cookieHeader = '') {
         return acc;
     }, {}); // The "{}" here is the initial value of the accumulator, which is an empty object.
 }
+
+
+
+
+/* **************************************************
+                       File Viewer
+   ************************************************** */
+
+let selectedFile = null; // Store the currently selected file.
+let currentPath = null; // Store the current path of a folder.
+
+// Select File
+/**  */
+function currentlySelectedFile(filePath) {
+    selectedFile = filePath;
+    console.log(`Selected file: ${selectedFile}`);
+}
+
+
+// Upload File
+
+
+
+// Download File
+
+
+
+// Create Folder
+/**  */
+function createFolder(folderName) { 
+    try {
+      if (!fs.existsSync(folderName)) {
+        fs.mkdirSync(folderName);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+}
+
+
+// Rename File
+/**  */
+function renameDirectory(oldDir, newDir) { // This would properly also include files
+    fs.rename(`${oldDir}`, `${newDir}`, err => {
+        if (err) {
+          console.error(err);
+        }
+        console.log(`Renamed to ${newDir}`);
+      });
+}
+
+
+// Move File
+/**  */
+function moveFile(sourcePath, destinationPath) {
+    const dest = path.join(destinationPath, path.basename(sourcePath));
+    fs.rename(sourcePath, dest, (err) => {
+        if (err) {
+            console.error("Error moving file:", err);
+        } else {
+            console.log(`File moved to ${dest}`);
+        }
+    });
+}
+
+
+// Function to delete a file
+/**  */
+function deleteFile(filePath) {
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error("Error deleting file:", err);
+        } else {
+            console.log(`File deleted at ${filePath}`);
+        }
+    });
+}
+
+// Function to delete a directory (including non-empty ones)
+/**  */
+function deleteDirectory(directoryPath) {
+    fs.rm(directoryPath, { recursive: true, force: true }, (err) => { // using recursive will enable deleting non-empty directories and force is to delete write-protected documents.
+        if (err) {
+            console.error("Error deleting directory:", err);
+        } else {
+            console.log(`Directory deleted at ${directoryPath}`);
+        }
+    });
+}
+
+
+
+// Navigate the file path
+/** Navigate the file path - in the future it would go more than one direction and implement a 'history'.
+ * @param {*} path is the path that you currently are on and is used for going backwards.
+ * @param {back} direction is used for whether the direction is going backwards or forward. Currently it can only go backwards, however with the 'history' implemented in the future, a forward direction could be implemented */
+function navigateFileDirection(path, direction) {
+    console.log(path); // Test
+
+    let pathElements = path.split('/'); // Splits at every /, turning the pathname into an array; example[] = {['This'],['is'],['an'],['example']}
+    let count = pathElements.length; // Get how many elements there are in the current path. example
+
+    let newPath; // Get the new path.
+    for (let i = 0; i - 1 < count; i++) {
+        newPath += path.join(pathElements[i]);
+    }
+
+    switch(direction) {
+        case 'back': {
+            fileState(path, newPath);
+            break;
+        }
+        default: {
+            console.log('Default was hit in navigateFileDirection');
+            break;
+        }
+
+    }
+}
+
+function fileState(oldPath, newPath) {
+    if (oldPath) closePath(oldPath);
+    fs.opendir(newPath, 'r+', )
+}
+
+function closePath(path) {
+    fs.closedir(path, err => {
+        if (err) throw err;
+    });
+}
--- node/PublicResources/js/file-viewer.js
+++ node/PublicResources/js/file-viewer.js
@@ -1,22 +1,10 @@
 
 /* **************************************************
-                    Impot & Export
+                    (o)._.(o)
    ************************************************** */
 
-export { FileTesting };
-import {  } from './server.js';
-import {  } from './app.js';
-
-import fs from 'fs'; // https://nodejs.org/en/learn/manipulating-files/working-with-folders-in-nodejs
-
-import path from 'path';
-import process from 'process';
-import { Pool } from 'pg';
-
-
-const publicResources = '/node/PublicResources/'; // Change to '../PublicResources/' on Ubuntu.
-const rootFileSystem = process.cwd(); // The path to the project (P2_Project).
-let selectedFile = null; // Store the selected file
+/* const publicResources = '/node/PublicResources/Files/'; // Change to '../PublicResources/Files/' on Ubuntu.
+const rootFileSystem = process.cwd(); // The path to the project (P2_Project). */
 
 // TEST: - Lortet virker ikke
 function FileTesting(path) {
@@ -26,82 +14,6 @@ function FileTesting(path) {
 
     console.log(fs.readdirSync(folderPath));
 }
+console.log("Hello?????");
 document.getElementById('fileTester').addEventListener('click', FileTesting('C:/Users/Emil/.ssh/'));
-
-// Upload File
-
-
-
-// Download File
-
-
-
-// Create Folder
-function createFolder(folderName) { 
-    try {
-      if (!fs.existsSync(folderName)) {
-        fs.mkdirSync(folderName);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-}
-
-
-// Rename File
-function renameDirectory(oldDir, newDir) { // This would properly also include files
-    fs.rename(`${oldDir}`, `${newDir}`, err => {
-        if (err) {
-          console.error(err);
-        }
-        console.log(`Renamed to ${newDir}`);
-      });
-}
-
-
-// Move File
-function moveFile(sourcePath, destinationPath) {
-    const dest = path.join(destinationPath, path.basename(sourcePath));
-    fs.rename(sourcePath, dest, (err) => {
-        if (err) {
-            console.error("Error moving file:", err);
-        } else {
-            console.log(`File moved to ${dest}`);
-        }
-    });
-}
-
-
-// Function to delete a file
-function deleteFile(filePath) {
-    fs.unlink(filePath, (err) => {
-        if (err) {
-            console.error("Error deleting file:", err);
-        } else {
-            console.log(`File deleted at ${filePath}`);
-        }
-    });
-}
-
-// Function to delete a directory (including non-empty ones)
-function deleteDirectory(directoryPath) {
-    fs.rm(directoryPath, { recursive: true, force: true }, (err) => { // using recursive will enable deleting non-empty directories and force is to delete write-protected documents.
-        if (err) {
-            console.error("Error deleting directory:", err);
-        } else {
-            console.log(`Directory deleted at ${directoryPath}`);
-        }
-    });
-}
-
-
-
-// Select File
-function selectFile(filePath) {
-    selectedFile = filePath;
-    console.log(`Selected file: ${selectedFile}`);
-}
-
-
-// Navigate the file path
 
